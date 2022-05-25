ce1057d81684eaf520d556dc9bd790ca3b4fdb0b
Vlee <me@vlee.dev>
2022-05-25 22:29:11 +0700

    Add alias docker-compose that uses docker-compose-plugin

--- aliases.sh
+++ aliases.sh
@@ -4,5 +4,7 @@ thisFilePath="$(readlink -f "${(%):-%x}")"
 workDir=$(dirname "${thisFilePath}")
 # Load lib
 source "${workDir}/lib.sh"
+# Alias for "docker-compose-plugin"
+setAlias 'docker-compose' 'docker compose'
 # Alias for docker-compose
 setAlias 'dkc' 'docker-compose'
