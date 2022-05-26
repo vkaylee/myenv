08d78921fe8594d1155f380d7152e1bdd2b65fe7
Vlee <me@vlee.dev>
2022-05-26 07:08:40 +0700

    Remove alias docker-compose that uses docker-compose-plugin

--- aliases.sh
+++ aliases.sh
@@ -4,7 +4,5 @@ thisFilePath="$(readlink -f "${(%):-%x}")"
 workDir=$(dirname "${thisFilePath}")
 # Load lib
 source "${workDir}/lib.sh"
-# Alias for "docker-compose-plugin"
-setAlias 'docker-compose' 'docker compose'
 # Alias for docker-compose
 setAlias 'dkc' 'docker-compose'
