26646639bb120deabda7c7cd71999cc3e94bc855
Lee Tuan <me@vlee.dev>
2023-03-26 14:39:50 +0700

    Change func name from rootNeeded to check_run_as_root

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ workDir=$(dirname "${thisFilePath}")
 # Load lib
 source "${workDir}/lib.sh"
 # Need to run as root
-rootNeeded # The function in lib.sh
+check_run_as_root "You must run this script by root permission" # The function in lib.sh
 # Set up zsh
