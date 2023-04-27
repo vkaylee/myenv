5b966014191dfeae7818d66dab3fdae0d92e6c87
Lee Tuan <me@vlee.dev>
2023-04-27 12:16:54 +0700

    Add aws cli

--- aliases.sh
+++ aliases.sh
@@ -8,3 +8,15 @@ source "${work_dir}/lib.sh"
 set_command_alias 'gshell' 'gcloud cloud-shell ssh --authorize-session'
 set_command_alias 'docker-compose' 'docker compose'
 set_command_alias 'dkc' 'docker-compose'
+# Some commands depend on docker cli and docker engine
+if [ "$(command -v docker)" ]; then
+    # AWS CLI
+    # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html
+    # Set AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_DEFAULT_REGION in .env file first
+    # Test by running in a new console session: aws configure list
+    if [[ -z "${AWS_ACCESS_KEY_ID}" || -z "${AWS_SECRET_ACCESS_KEY}" || -z "${AWS_DEFAULT_REGION}" ]]; then
+        echo "Please set AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_DEFAULT_REGION in .env file to use aws cli (amazon command line)"
+    else
+        set_command_alias 'aws' 'docker run --rm -it -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION public.ecr.aws/aws-cli/aws-cli:latest'
+    fi
+fi
