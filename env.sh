59d5bb450eb79114444bce36768ecd2683576628
Lee Tuan <me@vlee.dev>
2023-05-24 16:07:17 +0700

    Load custom .env.myenv in workspace directory

--- env.sh
+++ env.sh
@@ -2,10 +2,19 @@
 # Take working directory
 this_file_path="$(readlink -f "${(%):-%x}")"
 work_dir=$(dirname "${this_file_path}")
-if [ -r "${work_dir}/.env" ]; then
-  set -a
-  # shellcheck source=currentDir/.env
-  source <(sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g" < "${work_dir}/.env")
-  set +a
-  echo "Some environments in ${work_dir}/.env have been set!"
-fi
+myenv_set_envs_876892765834465872652357846459283659(){
+  local envFilePath=$1
+  if [ -r "${envFilePath}" ]; then
+    set -a
+    # shellcheck source=currentDir/.env
+    source <(sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g" < "${envFilePath}")
+    set +a
+    echo "Some environments in ${envFilePath} have been set!"
+  fi
+}
+
+# Load root ENV
+myenv_set_envs_876892765834465872652357846459283659 "${work_dir}/.env"
+
+# Load workspace ENV
+myenv_set_envs_876892765834465872652357846459283659 "$(pwd)/.env.myenv"
