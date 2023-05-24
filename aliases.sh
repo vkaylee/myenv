913e3b7fd15254f1836ac49e00881e8fd3d9320e
Lee Tuan <me@vlee.dev>
2023-05-24 11:54:41 +0700

    Add google cloud alias

--- aliases.sh
+++ aliases.sh
@@ -23,4 +23,7 @@ if [ "$(command -v docker)" ]; then
     # Add terraform
     set_command_alias 'terraform' "docker run --rm -ti --name terraform -w /workspace -v \$(pwd):/workspace -v ${work_dir}/appconfig/terraform:/root/.terraform.d hashicorp/terraform:latest"
     set_command_alias 'tf' 'terraform'
+    # Add google cloud cli
+    set_command_alias 'gcloud' "docker run --rm -ti --name gcloud -e CLOUDSDK_CONFIG=/config/mygcloud -v ${work_dir}/appconfig/google_cloud/mygcloud:/config/mygcloud -v ${work_dir}/appconfig/google_cloud:/certs gcr.io/google.com/cloudsdktool/google-cloud-cli gcloud"
+    set_command_alias 'gcli' 'gcloud'
 fi
