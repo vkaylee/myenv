3b0b14ddfb700ceeef4d698dbe693941dfb241c1
Lee Tuan <me@vlee.dev>
2023-05-25 14:28:40 +0700

    Add loading custom_scripts

--- import.sh
+++ import.sh
@@ -14,3 +14,9 @@ source "${work_dir}/detect.sh"
 source "${work_dir}/aliases.sh"
 # MyContainer
 source "${work_dir}/mycontainer.sh"
+
+# Load all custom scripts, the list will be ordered by name
+find "${work_dir}/custom_scripts/" -maxdepth 1 -type f -name "*.sh" -printf '%h\0%d\0%p\n' | sort -t '\0' -n | awk -F '\0' '{print $3}' | while read custom_script_path
+do
+    source "${custom_script_path}"
+done
