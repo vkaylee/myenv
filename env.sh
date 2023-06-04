a3f170b5e1635a958050d948a63f9e203b2df017
Lee Tuan <me@vlee.dev>
2023-06-04 13:22:25 +0700

    Sync all env.sh funcs with prefix myenv_env_876892765

--- env.sh
+++ env.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env zsh
-# Take working directory
-this_file_path="$(readlink -f "${(%):-%x}")"
-work_dir=$(dirname "${this_file_path}")
 # Associate array to store env by key value, key is unique
-typeset -A myenv_env_assarr_876892765834465872652357846459283659=()
+typeset -A myenv_env_876892765_env_assarr=()
 # Array for storing all env file paths
-myenv_env_file_paths_876892765834465872652357846459283659=()
-myenv_read_envs_876892765834465872652357846459283659(){
+myenv_env_876892765_env_file_paths=()
+myenv_env_876892765_read_envs(){
   local filePath="${1-}"
   while read -r line; do
     echo "${line}"
   done <<< "$(sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g" < "${filePath}")"
 }
-myenv_show_envs_876892765834465872652357846459283659(){
+myenv_env_876892765_show_envs(){
   local envLines=()
   # Take key value and format "key=value\n" for sorting
-  for envKey envValue in "${(@kv)myenv_env_assarr_876892765834465872652357846459283659}"; do
+  for envKey envValue in "${(@kv)myenv_env_876892765_env_assarr}"; do
     envLines+=( "${envKey}=${envValue}\n" )
   done
 
@@ -32,14 +29,14 @@ myenv_show_envs_876892765834465872652357846459283659(){
   # Print env file paths
   local envFileCount=0
   myenv_lib_983459816_typing_style_print "Envs are loaded from:"; printf "\n"
-  for envPath in "${myenv_env_file_paths_876892765834465872652357846459283659[@]}" ; do
+  for envPath in "${myenv_env_876892765_env_file_paths[@]}" ; do
     if [[ -n "${envPath}" ]]; then
       ((envFileCount++))
       myenv_lib_983459816_typing_style_print "- (${envFileCount}) ${envPath}"; printf "\n"
     fi
   done
 }
-myenv_set_envs_876892765834465872652357846459283659(){
+myenv_env_876892765_set_envs(){
   local envFilePath=$1
   if [ -r "${envFilePath}" ]; then
     local envLines=()
@@ -51,14 +48,14 @@ myenv_set_envs_876892765834465872652357846459283659(){
       envKey=$(echo "${envLine}" | grep -oP '^[^=]+')
       if [[ -n "${envKey}" ]]; then
         # Take value by removing the envKey
-        # Set key value to myenv_env_assarr_876892765834465872652357846459283659
-        myenv_env_assarr_876892765834465872652357846459283659["${envKey}"]="${envLine#"${envKey}="}"
+        # Set key value to myenv_env_876892765_env_assarr
+        myenv_env_876892765_env_assarr["${envKey}"]="${envLine#"${envKey}="}"
       fi
-    done < <(myenv_read_envs_876892765834465872652357846459283659 "${envFilePath}")
+    done < <(myenv_env_876892765_read_envs "${envFilePath}")
     set -a
     # shellcheck source=currentDir/.env
     if source <(echo -e "${envLines}"); then
-      myenv_env_file_paths_876892765834465872652357846459283659+=("${envFilePath}")
+      myenv_env_876892765_env_file_paths+=("${envFilePath}")
     fi
     set +a
     myenv_lib_983459816_typing_style_print "Some environment variables in ${envFilePath} have been set!"
@@ -67,7 +64,7 @@ myenv_set_envs_876892765834465872652357846459283659(){
 }
 
 # Load root ENV
-myenv_set_envs_876892765834465872652357846459283659 "${work_dir}/.env"
+myenv_env_876892765_set_envs "${MYENV_DIR}/.env"
 # Custom ENV
 ## appconfig DIR
 export MYENV_APPCONFIG_DIR="${MYENV_DIR}/appconfig"
@@ -75,4 +72,4 @@ export MYENV_APPCONFIG_DIR="${MYENV_DIR}/appconfig"
 touch ${MYENV_APPCONFIG_DIR}/kubeconfig/config
 export KUBECONFIG="${KUBECONFIG}:${MYENV_APPCONFIG_DIR}/kubeconfig/config"
 # Load workspace ENV
-myenv_set_envs_876892765834465872652357846459283659 "$(pwd)/.env.myenv"
+myenv_env_876892765_set_envs "$(pwd)/.env.myenv"
