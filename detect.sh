5bae6104b3b4a3b2a8ad5170a223aac477fa7822
Lee Tuan <me@vlee.dev>
2023-06-04 18:45:38 +0700

    Add detecting package manager to MYENV_PACKAGE_MANAGER

--- detect.sh
+++ detect.sh
@@ -1,3 +1,35 @@
 #!/usr/bin/env zsh
-# Take working directory
-this_file_path="$(readlink -f "${(%):-%x}")"
+if [[ -n "${MYENV_PACKAGE_MANAGER}" ]]; then
+  return
+fi
+
+# Check if package manager exists
+myenv_detect_2989829823_check_package_manager() {
+  command -v "${1}" >/dev/null 2>&1
+}
+
+# Package managers to check
+myenv_detect_2989829823_package_managers=(
+  "apt"     # Debian/Ubuntu
+  "apt-get" # Debian/Ubuntu
+  "dnf"     # Fedora/RHEL (Red Hat Enterprise Linux)/CentOS/AlmaLinux/Rocky/Oracle/Mageia/OpenMandriva/PCLinuxOS/Scientific Linux
+  "yum"     # Fedora/RHEL (Red Hat Enterprise Linux)/CentOS/AlmaLinux/Rocky/Oracle/Mageia/OpenMandriva/PCLinuxOS/Scientific Linux
+  "zypper"  # openSUSE/SUSE Linux Enterprise/GeckoLinux/Tumbleweed/Leap
+  "pacman"  # Arch Linux/Manjaro/EndeavourOS/Garuda Linux/ArcoLinux/Artix Linux
+  "emerge"  # Gentoo Linux/Funtoo Linux
+  "urpmi"   # Mageia/OpenMandriva/PCLinuxOS
+  "nix-env" # NixOS/Guix System
+  "pkg"     # FreeBSD/NetBSD/DragonFly BSD/OpenBSD
+  "pkgutil" # Solaris 9/10/11 (OpenCSW)
+  "pkg_add" # OpenBSD
+  "apk"     # Alpine
+  "tazpkg"  # Slitaz
+)
+
+# Iterate over package managers and check their availability
+for manager in "${myenv_detect_2989829823_package_managers[@]}"; do
+  if myenv_detect_2989829823_check_package_manager "${manager}"; then
+    export MYENV_PACKAGE_MANAGER="${manager}"
+    break
+  fi
+done
