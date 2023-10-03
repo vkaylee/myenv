7972323a0742a5b2887a0590eb1a0a74a6058820
Lee Tuan <me@vlee.dev>
2023-10-03 22:47:16 +0700

    Customize to be able to run test in local

--- ci_test.sh
+++ ci_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 input_user="$1"
 file_dir_url="$2"
-sudo su -c "export fileDirUrl=\"${file_dir_url}\"; bash <(curl -sSL \"\${fileDirUrl}/setup.sh?\$(date +%s)\")" "${input_user}"
+sudo su -c "export fileDirUrl=\"${file_dir_url}\"; bash <(curl -sSL \"\${fileDirUrl}/setup.sh?\$(date +%s)\") test" "${input_user}"
---
+++ test.local.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+# Run the test with the input is one of 
+oses=()
+test_oses=("debian" "ubuntu" "fedora")
+# if input is not empty
+if [[ -n "${1}" ]]; then
+    for test_os in "${test_oses[@]}"; do
+        if [[ "${1}" == "${test_os}" ]]; then
+            oses+=("${1}")
+        fi
+    done
+else
+    for test_os in "${test_oses[@]}"; do
+        oses+=("${test_os}")
+    done
+fi
+
+trap "docker compose down" EXIT
+
+for os in "${oses[@]}"; do
+    echo "Test for ${os}"
+    docker compose down
+    docker compose build "${os}"
+    docker compose up -d "${os}"
+    docker compose exec "${os}" /ci_test.sh docker http://file-server
+done
+
